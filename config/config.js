912b7e405f7af32b534ea5e2115fe9aae37d97d1
maria <mariagiraldo4@gmail.com>
2019-10-24 18:32:44 -0500

    changes

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 //const STRING_CONNECTION = "mongodb://app:1234@localhost:27017/microcredits_db"
-const STRING_CONNECTION = "mongodb://admin:Admin123@ds137508.mlab.com:37508/microcredits_db"
-//const STRING_CONNECTION  = "mongodb+srv://admin:Admin123@cluster0-2zum3.mongodb.net/microcredits_bd"
+//const STRING_CONNECTION = "mongodb://admin:Admin123@ds137508.mlab.com:37508/microcredits_db"
+const STRING_CONNECTION  = "mongodb+srv://admin:Admin123@cluster0-2zum3.mongodb.net/microcredits_bd"
 
 module.exports = {
     PORT: process.env.PORT || 3000,
